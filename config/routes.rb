a18dfd3af5eb58511980f01a7a7c4220ca8427f2
川北圭祐 <kawakita.keisuke.infratop@gmail.com>
2020-03-18 14:38:39 +0900

    [Add] 5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'users#top'
   get 'home/about' => 'users#about', as: 'about_book'
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'devise/sessions', 
+  	registrations: 'devise/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :edit, :update]
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
